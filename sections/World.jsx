7c3670229351018e887fbf2cccca9eff56ec93f7
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-01-02 23:23:48 -0800

    Update World.jsx
    updated people icon

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,10 +1,10 @@
 'use client';
-
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { TitleText, TypingText } from '../components';
-import { fadeIn, staggerContainer } from '../utils/motion';
+
+import {  TitleText, TypingText } from '../components';
+import { planetVariants, planetVariants2, staggerContainer, fadeIn,zoomIn } from '../utils/motion';
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -13,38 +13,65 @@ const World = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    ></motion.div>
+    <TypingText title='| People on the world' textStyles='text-center'/>
+  <TitleText title={<>Track Friends around you and inite them to play togeth in the same world</>}
+  textStyles='text-center'
+  
+  />
+  <motion.div
+  variants={fadeIn('up','tween',0.3,1)}
+  className='relative mt-[68px] flex w-full h-[550px]'
+  
+  
+  >
 
-      <TypingText title="| People on the World" textStyles="text-center" />
-      <TitleText
-        title={(
-          <>Track friends around you and invite them to play together in the same
-            world
-          </>
-        )}
-        textStyles="text-center"
+
+    <img src="/map.png" alt="map"
+    className='w-full h-full object-cover'
+    
+    />
+    <motion.div
+      variants={zoomIn('up','tween',0.3,1)}
+    className='absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-01.png' alt='people'
+      className='w-full h-full'
       />
-
-      <motion.div
-        variants={fadeIn('up', 'tween', 0.3, 1)}
-        className="relative mt-[68px] flex w-full h-[550px]"
-      >
-        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="people-01.png" alt="people" className="w-full h-full" />
-        </div>
-
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="/people-02.png" alt="people" className="w-full h-full" />
-        </div>
-
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="people-03.png" alt="people" className="w-full h-full" />
-        </div>
-      </motion.div>
     </motion.div>
+    
+    <div className='absolute top-10 left-20  w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-02.png' alt='people'
+      className='w-full h-full'
+      />
+    </div>
+    <div className='absolute top-1/2 right-20  w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-02.png' alt='people'
+      className='w-full h-full'
+      />
+    </div>
+    <div className='absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-03.png' alt='people'
+      className='w-full h-full'
+      />
+    </div>
+   < div className='absolute top-1/1 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-03.png' alt='people'
+      className='w-full h-full'
+      />
+    </div>
+    <div className='absolute top-1/2 left-[4%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+      <img
+      src='people-01.png' alt='people'
+      className='w-full h-full'
+      />
+    </div>
+  </motion.div> 
   </section>
 );
 
