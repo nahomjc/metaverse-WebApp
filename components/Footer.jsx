90ef0155214588ad5b60583d264cfccb8d7013ff
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-01-02 22:34:32 -0800

    removed button

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,17 +19,7 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Enter the Metaverse
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-        /* eslint-disable eol-last */
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="w-[24px] h-[24px] object-contain"
-          />
-          <span className="font-normal text-[16px] text-white">
-            Enter Metaverse
-          </span>
-        </button>
+       
       </div>
 
       <div className="flex flex-col">
